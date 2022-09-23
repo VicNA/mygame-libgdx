065256a611d6095edc41b8979621443b52af11f7
viktor-n <vnikolaeff989@gmail.com>
2022-09-23 17:01:26 +0300

    lesson 1 - init

--- core/src/ru/geekbrains/mygdx/game/MyGdxGame.java
+++ core/src/ru/geekbrains/mygdx/game/MyGdxGame.java
@@ -1,31 +1,46 @@
 package ru.geekbrains.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	MyAnimation anim;
+//	Texture coinImg;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+//        img = new Texture("badlogic.jpg");
+//		coinImg = new Texture("Full Coinss.png");
+		anim = new MyAnimation("explosion.png", 4, 8, 1f, Animation.PlayMode.LOOP);
+
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+	public void render() {
+		ScreenUtils.clear(1, 1, 1, 1);
+
+		anim.setTime(Gdx.graphics.getDeltaTime());
+
+		float x = Gdx.input.getX() - anim.draw().getRegionWidth() / 2;
+		float y = Gdx.graphics.getHeight() - (Gdx.input.getY() + anim.draw().getRegionHeight() / 2);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(anim.draw(), x, y);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
+//        img.dispose();
+//		coinImg.dispose();
+		anim.dispose();
 	}
 }
